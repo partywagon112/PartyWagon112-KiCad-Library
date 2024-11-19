5b8e003616d8502f5d4ba7b4df99406598345778
partywagon112 <patrick.curtain.112@outlook.com>
2024-11-19 12:36:20 +1100

    Initial Commit v2

--- add_kicad-latest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir kicad-latest
-
-git submodule add "https://gitlab.com/kicad/libraries/kicad-symbols.git" kicad-latest/symbols
-git submodule add "https://gitlab.com/kicad/libraries/kicad-footprints.git" kicad-latest/footprints
-git submodule add "https://gitlab.com/kicad/libraries/kicad-packages3d.git" kicad-latest/3dmodels
